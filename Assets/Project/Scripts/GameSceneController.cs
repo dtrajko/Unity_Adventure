930f2b1b998a5d5975a94259b2910e5acf70b4e2
Dejan Trajkovic <dtrajko@gmail.com>
2019-05-06 03:45:59 +0200

    Section 8 Camera & UI - 54. Game Interface part 2

---
+++ Assets/Project/Scripts/GameSceneController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameSceneController : MonoBehaviour
+{
+    [Header("Game")]
+    public Player player;
+
+    [Header("UI")]
+    public Text healthText;
+    public Text bombsText;
+    public Text arrowsText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int playerHealth = player != null ? player.health : 0;
+        healthText.text = "Health: " + playerHealth.ToString();
+
+        int bombAmount = player != null ? player.bombAmount : 0;
+        bombsText.text = "Bombs: " + bombAmount.ToString();
+
+        int arrowAmount = player != null ? player.arrowAmount : 0;
+        arrowsText.text = "Arrows: " + arrowAmount.ToString();
+    }
+}
