b1ded12f103a58fd8397af8cbc50c04b6518e1e5
Dejan Trajkovic <dtrajko@gmail.com>
2019-05-09 03:11:40 +0200

    Section 10 Level Design - 63. Dungeon Entrance (complete)

--- Assets/Project/Scripts/Game/GameCamera.cs
+++ Assets/Project/Scripts/Game/GameCamera.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameCamera : MonoBehaviour
 {
     public GameObject target;
-    public Vector3 offset = new Vector3(0, 3, -3);
+    public Vector3 offset = new Vector3(0, 5, -15);
     public float focusSpeed = 3f;
 
     // Start is called before the first frame update
