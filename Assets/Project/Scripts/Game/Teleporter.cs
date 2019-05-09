a1ebc6728fb021aedecfe343f81ad103420233f6
Dejan Trajkovic <dtrajko@gmail.com>
2019-05-09 05:06:50 +0200

    Section 10 Level Design - 65. Dungeon

--- Assets/Project/Scripts/Game/Teleporter.cs
+++ Assets/Project/Scripts/Game/Teleporter.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Teleporter : MonoBehaviour
 {
     public Teleporter exitTeleporter;
-    public float exitOffset = 2f;
+    public float exitOffset = 4f;
     void OnTriggerEnter(Collider otherCollider) {
         if (otherCollider.GetComponent<Player>() != null) {
             Debug.Log("The Player entered the Teleporter!");
