602f8de038e2f12685d18a48ed968f8fbd19663f
Dejan Trajkovic <dtrajko@gmail.com>
2019-06-12 01:25:31 +0200

    Section 22: Effects and AI - 167. Agent Animation

--- Assets/Project/Scripts/Game/Enemies/ChasingEnemy.cs
+++ Assets/Project/Scripts/Game/Enemies/ChasingEnemy.cs
@@ -1,18 +1,24 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class ChasingEnemy : MonoBehaviour
+public class ChasingEnemy : Enemy
 {
     public NavMeshAgent agent;
     public Animator enemyAnimator;
     public float movementRadius = 15f;
-    public float movementUpdateDuration = 3f;
+    public float movementUpdateDuration = 6f;
 
     private Vector3 initialPosition;
     private float movementUpdateTimer;
+    private Vector3 originalEnemyAnimatorPosition;
+    private Vector3 previousPosition;
+
+    void Awake() {
+        previousPosition = transform.position;
+        originalEnemyAnimatorPosition = enemyAnimator.transform.localPosition;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -30,14 +36,30 @@ public class ChasingEnemy : MonoBehaviour
             movementUpdateTimer = movementUpdateDuration;
             MoveAroundStart();
         }
+
+        if (agent.velocity.magnitude > 0) {
+            enemyAnimator.SetFloat("Forward", 0.6f);
+        } else {
+            enemyAnimator.SetFloat("Forward", 0.0f);
+        }
+
+        if (Vector3.Distance(transform.position, previousPosition) < 0.03f) {
+            enemyAnimator.SetFloat("Forward", 0.0f);
+        }
+
+        previousPosition = transform.position;
     }
 
     void MoveAroundStart()
     {
         agent.SetDestination(initialPosition + new Vector3(
-            UnityEngine.Random.Range(-movementRadius, movementRadius),
+            Random.Range(-movementRadius, movementRadius),
             0,
-            UnityEngine.Random.Range(-movementRadius, movementRadius)
+            Random.Range(-movementRadius, movementRadius)
         ));
+    }
+
+    void LateUpdate() {
+        enemyAnimator.transform.localPosition = originalEnemyAnimatorPosition;
     }
 }
